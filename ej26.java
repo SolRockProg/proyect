4213cbece9d5f518f90a9218778c6f52e723bfd1
SolRockProg <35456768+SolRockProg@users.noreply.github.com>
2018-02-14 20:25:01 +0100

    Update ej26.java

--- ej26.java
+++ ej26.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class ej26 {
-	public static int obtenerPuntos(String[][] matriz, String[] equipos, String nomeq) {
+	public static int obtenerPuntos(char[][] matriz, String[] equipos, String nomeq) {
 		int eq = 0;
 		for (int i = 0; i < equipos.length; i++) {
 			if (equipos[i] == nomeq)
@@ -10,19 +10,19 @@ public class ej26 {
 		int puntos=0;
 		for (int t=0; t<matriz.length; t++){
 			switch(matriz[eq][t]){
-			case "1":
+			case '1':
 				puntos+=3;
 				break;
-			case "X": 
+			case 'X': 
 				puntos+=1;
 				break;
 				
 			}
 			switch(matriz[t][eq]){
-			case"2":
+			case'2':
 				puntos+=3;
 				break;
-			case"X":
+			case'X':
 				puntos+=1;
 				break;
 			}
@@ -32,11 +32,14 @@ public class ej26 {
 
 	public static void main(String[] args) throws FileNotFoundException{
 		String[] vector = {"vicente","culo_caliente","sanguich","arriba_yugoslavia"};
-		String[][] matriz= {{"-","2","1", "1"}, {"1", "-", "1", "1"}, {"1", "2","-","1"}, {"1", "2","1", "-"}};
+		char[][] matriz= {{'-','2','1', '1'}, {'1', '-', '1', '1'}, {'1', '2','-','1'}, {'1', '2','1', '-'}};
 			System.out.println(obtenerPuntos(matriz, vector,"arriba_yugoslavia" ));
 		}
 	
 	}
+	
+
+
 	
 
 

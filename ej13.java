d0fad7937c7cc32b56a6d17773a3d67b79e55584
SolRockProg <35456768+SolRockProg@users.noreply.github.com>
2018-02-14 18:25:09 +0100

    Update ej13.java

--- ej13.java
+++ ej13.java
@@ -1,27 +1,29 @@
-
-
 public class ej13 {
-	public static String obtenerPalabras(String cat, int n) {
-		int palabras=0;
-		String newP="";
-		for (int t=0; t<cat.length()-1; t++) {
-			if (cat.charAt(t)!=' ' && palabras==0) {
-				palabras++;
-				
+	public static String obtenerPalabras(String cat, int i) {
+		int palabras = 0;
+		String newP = "";
+		if (i <= cat.length()-1) {
+			for (int t = 0; t < cat.length(); t++) {
+				if (cat.charAt(t) != ' ' && palabras == 0) {
+					palabras++;
+
+				}
+				if (cat.charAt(t) == ' ' && cat.charAt(t + 1) != ' ') {
+					palabras++;
+				}
+				if (palabras == i && cat.charAt(t)!=' ') {
+					newP += cat.charAt(t);
+				}
+
 			}
-			if (cat.charAt(t)==' ' && cat.charAt(t+1)!=' ') {
-				palabras++;
-			}
-			if (palabras==n) {
-				newP+=cat.charAt(t);
-			}
-			
-		}
-		return newP;
+			return newP;
+		} else
+			return null;
+
 	}
 
 	public static void main(String[] args) {
-		System.out.print(obtenerPalabras("Tengo una oya llena de kk",1));
+		System.out.print(obtenerPalabras("hola muy buenas tardes", 3));
 
 	}
 }
